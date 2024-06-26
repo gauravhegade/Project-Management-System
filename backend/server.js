3680d0dbda64cbab030d35b8c8b8210f52621bab
ankithshetty397 <ankithpshetty@gmail.com>
2024-06-26 15:45:34 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)     // connect("mongodb://localhost:27017/LOCALDB", {})
     .then(()=>{
         app.listen(process.env.PORT,()=>{
             console.log('Connected to MongoDB');
